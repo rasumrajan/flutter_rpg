f12dde07e134ba1eb641947d6b8d522ad2971a1d
rasumrajan <coolrasum@gmail.com>
2025-05-01 21:08:18 +0530

    character rpg

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your characters'),
+        centerTitle: true,
+      ),
+          body: Container(
+            padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text('Charater List'),
+                  FilledButton(onPressed: (){}, child: Text('Create New'),)
+                ],
+              )
+          ),
+    );
+  }
+}
